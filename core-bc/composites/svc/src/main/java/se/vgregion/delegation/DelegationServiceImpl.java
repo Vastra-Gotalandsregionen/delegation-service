f936e7f898ea1b256e9f364463a7c7929ff1454f
wmdaros <wmdaros@8c51f7aa-c060-7f12-ce57-3d08bcd20b85>
2011-10-10 15:03:15 +0000

    - DelegationService - Remove group delegation

---
+++ core-bc/composites/svc/src/main/java/se/vgregion/delegation/DelegationServiceImpl.java
@@ -0,0 +1,57 @@
+package se.vgregion.delegation;
+
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import se.vgregion.delegation.domain.Delegation;
+import se.vgregion.delegation.persistence.DelegationRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: david
+ * Date: 10/10-11
+ * Time: 16:20
+ */
+public class DelegationServiceImpl implements DelegationService {
+    @Autowired
+    private DelegationRepository delegationRepository;
+
+    @Override
+    public List<Delegation> delegatedBy(String vcVgrId) {
+        return new ArrayList<Delegation>();
+    }
+
+    @Override
+    public List<Delegation> delegatedTo(String vgrId) {
+        return new ArrayList<Delegation>();
+    }
+
+    @Override
+    public List<Delegation> delegationsInProgress(String vcVgrId) {
+        return new ArrayList<Delegation>();
+    }
+
+    @Override
+    public boolean approve(Delegation delegation, String signToken) {
+        return false;
+    }
+
+    @Override
+    public Delegation extend(Long oldDelegationId, String createdBy, DateTime newFrom, DateTime newTo) {
+        Delegation newDelegation = new Delegation();
+        return newDelegation;
+    }
+
+    /**
+     * Validate that signToken is signed by signer.
+     *
+     * @param delegation
+     * @param signToken
+     * @return
+     */
+    private boolean validateSignToken(Delegation delegation, String signToken) {
+        return false;
+    }
+}
